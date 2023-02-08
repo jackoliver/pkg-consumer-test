89f974eba260e42aa5118f11f86f83686f69f60c
Jack Oliver <jeoliver@cloudbees.com>
2023-02-08 13:50:44 +0100

    Stashy mooose-tache

--- apps/pkg-consumer/src/app/app.tsx
+++ apps/pkg-consumer/src/app/app.tsx
@@ -1,16 +1,26 @@
-import styled from '@emotion/styled';
-import NxWelcome from './nx-welcome';
-
-const StyledApp = styled.div`
-  // Your style here
-`;
-
+// @ts-expect-error no defs
+import { MuiTest } from '@jvla/mui-test-pkg';
+// @ts-expect-error no defs
+import { ContextTest, useContextTest } from '@jvla/context-test-pkg';
 export function App() {
   return (
-    <StyledApp>
-      <NxWelcome title="pkg-consumer" />
-    </StyledApp>
+    <ContextTest>
+      <MuiTest>
+        <ConnectedComponent />
+      </MuiTest>
+    </ContextTest>
   );
 }
+
+const ConnectedComponent = () => {
+  const { active } = useContextTest();
+  return (
+    <div>
+      {active.get() ? 'active' : 'inactive'}
+      <br />
+      <button onClick={() => active.set(!active.get())}>Toggle</button>
+    </div>
+  );
+};
 
 export default App;
